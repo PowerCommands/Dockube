7b9eb7c3bfeab386860fdb5b18508cae6acc0753
Harri Klingsten <harri.klingsten2@scb.se>
2023-05-04 22:40:58 +0200

    Removed git server, replaced with gogs git server.

---
+++ src/PowerCommands/PainKiller.Dockube/Dockube/DockubeCommands/Commands/KCommand.cs
@@ -0,0 +1,12 @@
+using PainKiller.PowerCommands.Core.Commands;
+
+namespace DockubeCommands.Commands;
+
+[PowerCommandTest(         tests: " ")]
+[PowerCommandDesign( description: "Run kubectl commands using the alias k",
+              overrideHelpOption: true,
+                         example: "k")]
+public class KCommand : MasterCommando
+{
+    public KCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration, alias: "kubectl") { }
+}
--- src/PowerCommands/PainKiller.Dockube/Dockube/DockubeCommands/Commands/StartupCommand.cs
+++ src/PowerCommands/PainKiller.Dockube/Dockube/DockubeCommands/Commands/StartupCommand.cs
@@ -4,6 +4,7 @@ using DockubeCommands.Managers;
 namespace DockubeCommands.Commands;
 
 [PowerCommandDesign( description: "Startup your Dockube environment",
+                         options: "confirm",
                          example: "startup")]
 public class StartupCommand : CommandBase<PowerCommandsConfiguration>
 {
@@ -11,15 +12,29 @@ public class StartupCommand : CommandBase<PowerCommandsConfiguration>
 
     public override RunResult Run()
     {
-        var fullFileName = Path.Combine(Configuration.PathToDockerDesktop, "Docker Desktop.exe");
-        ShellService.Service.Execute(fullFileName, arguments: "", workingDirectory: "", WriteLine, fileExtension: "");
-        Thread.Sleep(5000);
-        WriteSuccessLine("Docker Desktop started");
-
-        var workingDirectory = Path.Combine(AppContext.BaseDirectory, "Manifests", "argocd");
-
-        IPublishManager publisher = new PublishManager(workingDirectory);
-        publisher.Publish(workingDirectory, "argocd");
+        var confirmed = !HasOption("confirm") || DialogService.YesNoDialog("Do you want to start Docker desktop?");
+        if (confirmed)
+        {
+            WriteSuccessLine("Docker Desktop starting please wait...");
+            var fullFileName = Path.Combine(Configuration.PathToDockerDesktop, "Docker Desktop.exe");
+            ShellService.Service.Execute(fullFileName, arguments: "", workingDirectory: "", WriteLine, fileExtension: "");
+            Thread.Sleep(15000);
+            WriteSuccessLine("Docker Desktop started");
+        }
+        confirmed = !HasOption("confirm") || DialogService.YesNoDialog("Do you want to deploy ArgoCD?");
+        if (confirmed)
+        {
+            var workingDirectory = Path.Combine(AppContext.BaseDirectory, "Manifests", "argocd");
+            IPublishManager publisher = new PublishManager(workingDirectory);
+            publisher.Publish(workingDirectory, "argocd");
+        }
+        confirmed = !HasOption("confirm") || DialogService.YesNoDialog("Do you want to deploy Git server?");
+        if (confirmed)
+        {
+            var workingDirectory = Path.Combine(AppContext.BaseDirectory, "Manifests", "gogs");
+            IPublishManager publisher = new PublishManager(workingDirectory);
+            publisher.Publish(workingDirectory, "gogs");
+        }
         return Ok();
     }
 }
