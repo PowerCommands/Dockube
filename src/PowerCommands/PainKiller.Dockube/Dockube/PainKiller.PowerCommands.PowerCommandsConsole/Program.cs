3009aa4453624ff933f17ce6c40d85dcced33b25
Harri Klingsten <harri.klingsten2@scb.se>
2023-05-24 21:51:13 +0200

    Added a logo

--- src/PowerCommands/PainKiller.Dockube/Dockube/PainKiller.PowerCommands.PowerCommandsConsole/Program.cs
+++ src/PowerCommands/PainKiller.Dockube/Dockube/PainKiller.PowerCommands.PowerCommandsConsole/Program.cs
@@ -1,4 +1,9 @@
 ﻿using PainKiller.PowerCommands.Core.Services;
 
-ConsoleService.Service.WriteHeaderLine(nameof(Program), "Dockube Commands 1.0");
+ConsoleService.Service.WriteLine(nameof(Program), 
+
+@" ____  _____  ___  _  _  __  __  ____  ____     ___  __    ____    __    ___  
+(  _ \(  _  )/ __)( )/ )(  )(  )(  _ \( ___)   / __)(  )  (_  _)  /  )  / _ \ 
+ )(_) ))(_)(( (__  )  (  )(__)(  ) _ < )__)   ( (__  )(__  _)(_    )(  ( (_) )
+(____/(_____)\___)(_)\_)(______)(____/(____)   \___)(____)(____)  (__)()\___/ ",ConsoleColor.Cyan);
 PainKiller.PowerCommands.Bootstrap.Startup.ConfigureServices().Run(args);
