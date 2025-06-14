f9544ff382dfc4d5007312caf61315605a6c8678
Harri Klingsten <harri.klingsten2@scb.se>
2025-06-14 12:14:37 +0200

    Refactor and improved.

--- src/Client/PainKiller.DockubeClient/Commands/CoreCommand.cs
+++ src/Client/PainKiller.DockubeClient/Commands/CoreCommand.cs
@@ -1,22 +1,26 @@
+using PainKiller.CommandPrompt.CoreLib.Modules.ShellModule.Services;
+using System.Text;
+using PainKiller.CommandPrompt.CoreLib.Core.Services;
 using PainKiller.DockubeClient.Extensions;
 
 namespace PainKiller.DockubeClient.Commands;
 
-[CommandDesign(     description: "Manage the Core Dockube cluster", 
+[CommandDesign(description: "Manage the Core Dockube cluster",
                       arguments: ["<Mode>"],
                          quotes: ["<Namespace name>"],
-                        options: ["hosts", "pods", "secrets","tls","endpoints","ingress","namespaces"],
-                    suggestions: ["init","\"gitlab\"","\"observation\""],
-                       examples: ["//View status of your dockube platform","core","//Initialize the core platform","core init"])]
+                        options: ["hosts", "passwords", "pods", "secrets", "tls", "endpoints", "ingress", "namespaces"],
+                    suggestions: ["init", "\"gitlab\"", "\"observation\""],
+                       examples: ["//View status of your dockube platform", "core", "//Initialize the core platform", "core init"])]
 public class CoreCommand(string identifier) : ConsoleCommandBase<CommandPromptConfiguration>(identifier)
 {
     public override RunResult Run(ICommandLineInput input)
     {
-        if(this.GetSuggestion(input.Arguments.FirstOrDefault(),"") == "init") return Init();
+        if (this.GetSuggestion(input.Arguments.FirstOrDefault(), "") == "init") return Init();
+        if (input.HasOption("passwords")) return GetPasswords();
         StatusCoreCluster(input.Options, $"{input.Quotes.FirstOrDefault()}");
         return Ok();
     }
-    private RunResult StatusCoreCluster(IDictionary<string,string> options, string nameSpace)
+    private RunResult StatusCoreCluster(IDictionary<string, string> options, string nameSpace)
     {
         var namespaces = Configuration.Dockube.GetReleases().Where(r => r.IsCore && !string.IsNullOrWhiteSpace(r.Namespace)).Select(r => r.Namespace).Distinct().ToList();
         Writer.WriteHeadLine("Core Cluster Status");
@@ -29,37 +33,35 @@ public class CoreCommand(string identifier) : ConsoleCommandBase<CommandPromptCo
 
         foreach (var ns in namespaces.Where(n => n.Contains(nameSpace)))
         {
-            Writer.WriteHeadLine($"Namespace: {ns}");
-
             if (options.ContainsKey("pods") || options.Count == 0)
             {
-                Writer.WriteLine("Pods");
+                Writer.WriteLine($"Pods in namespace: {ns}");
                 RunCommand($"kubectl get pods -n {ns}", $"Pods in {ns}");
             }
             if (options.ContainsKey("secrets") || options.Count == 0)
             {
-                Writer.WriteLine("Secrets");
+                Writer.WriteLine($"Secrets in namespace {ns}");
                 RunCommand($"kubectl get secrets -n {ns}", $"Secrets in {ns}");
             }
 
             if (options.ContainsKey("tls") || options.Count == 0)
             {
-                Writer.WriteLine("TLS Secrets");
+                Writer.WriteLine($"TLS Secrets in namespace {ns}");
                 RunCommand($@"kubectl get secrets -n {ns} -o jsonpath=""{{range .items[?(@.type=='kubernetes.io/tls')]}}{{.metadata.name}}{{'\n'}}{{end}}""", $"TLS Secrets in {ns}");
             }
 
             if (options.ContainsKey("endpoints") || options.Count == 0)
             {
-                Writer.WriteLine("Endpoints");
+                Writer.WriteLine($"Endpoints in namespace {ns}");
                 RunCommand($"kubectl get endpoints -n {ns}", $"Endpoints in {ns}");
             }
 
             if (options.ContainsKey("ingress") || options.Count == 0)
             {
-                Writer.WriteLine("Ingress Resources");
+                Writer.WriteLine($"Ingress Resources in namespace {ns}");
                 RunCommand($"kubectl get ingress -n {ns}", $"Ingress in {ns}");
 
-                Writer.WriteLine("Ingress Hosts");
+                Writer.WriteLine($"Ingress Hosts in namespace {ns}");
                 RunCommand($@"kubectl get ingress -n {ns} -o jsonpath=""{{range .items[*]}}{{.metadata.name}} {{.spec.rules[*].host}}{{'\n'}}{{end}}""", $"Ingress Hosts in {ns}");
             }
         }
@@ -82,8 +84,41 @@ public class CoreCommand(string identifier) : ConsoleCommandBase<CommandPromptCo
     }
     private void ShowHosts()
     {
+        var releases = Configuration.Dockube.GetReleases().Where(r => r.IsCore).ToList();
+        foreach (var dockubeRelease in releases)
+        {
+            Console.WriteLine($"{dockubeRelease.Name}");
+        }
         var endpoints = Configuration.Dockube.GetReleases().Where(r => r.IsCore).SelectMany(r => r.Resources).Select(res => res.Endpoint).Where(e => !string.IsNullOrWhiteSpace(e)).Distinct().ToList();
         Writer.WriteHeadLine("Hosts");
         foreach (var endpoint in endpoints) Writer.WriteLine($"Endpoint: {endpoint}");
+    }
+
+    private RunResult GetPasswords()
+    {
+        var releases = Configuration.Dockube.GetReleases().Where(r => r.IsCore).ToList();
+        foreach (var release in releases)
+        {
+            var releaseName = release.Name;
+            if (string.IsNullOrEmpty(releaseName)) return Ok();
+            var resource = release.Resources.FirstOrDefault(r => r.SecretDescriptors.Length > 0);
+            if (resource != null)
+            {
+                var base64Encoded = ShellService.Default.StartInteractiveProcess("kubectl.exe", $"get secret {resource.SecretDescriptors.First().Key} -n {release.Namespace} -o jsonpath='{{.data.password}}'").Trim().Replace("'", "");
+                var decoded = Encoding.UTF8.GetString(Convert.FromBase64String(base64Encoded));
+                Console.WriteLine(releaseName);
+                Console.WriteLine(decoded);
+            }
+
+            var tokenCommandResource = release.Resources.FirstOrDefault(r => r.After.Any(a => a.ToLower().Contains("create token")));
+            var tokenCommand = tokenCommandResource?.After.FirstOrDefault(a => a.ToLower().Contains("create token"))?.Trim();
+            if (tokenCommand != null)
+            {
+                Console.WriteLine($"Access token for {releaseName} (may be short lived)");
+                var result = ShellService.Default.StartInteractiveProcess("cmd.exe", $"/c {tokenCommand}");
+                ConsoleService.Writer.WriteSuccessLine(result);
+            }
+        }
+        return Ok();
     }
 }
