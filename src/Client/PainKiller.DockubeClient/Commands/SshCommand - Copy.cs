fe942700dfb4d87aac20eddea370e0d12e70fc0e
Harri Klingsten <harri.klingsten2@scb.se>
2025-06-30 16:13:22 +0200

    .

---
+++ src/Client/PainKiller.DockubeClient/Commands/SshCommand - Copy.cs
@@ -0,0 +1,41 @@
+using PainKiller.CommandPrompt.CoreLib.Modules.SecurityModule.Extensions;
+using PainKiller.ReadLine.Managers;
+using Renci.SshNet;
+
+namespace PainKiller.DockubeClient.Commands;
+
+[CommandDesign(     description:  "Dockube -  Shutdown a specific server or everyone that starts with the name pi0",
+                       examples: ["//Shutdown every configured instance with hostname that starts with pi0","ssh --shutdown",
+                                  "//Shutdown specific ssh host","shutdown <host-name>"])]
+public class ShutdownCommand(string identifier) : ConsoleCommandBase<CommandPromptConfiguration>(identifier)
+{
+    private SshClient _client = null!;
+    private readonly string _identifier = identifier;
+
+    public override void OnInitialized()
+    {
+        SuggestionProviderManager.AppendContextBoundSuggestions(_identifier, Configuration.Dockube.Ssh.Select(s => s.Name).ToArray());
+        base.OnInitialized();
+    }
+    public override RunResult Run(ICommandLineInput input)
+    {
+        var target = input.Arguments.FirstOrDefault() ?? "";
+        return Shutdown(target);
+    }
+    public RunResult Shutdown(string target)
+    {
+        foreach (var config in Configuration.Dockube.Ssh.Where(s => s.Host.StartsWith("pi0"))) 
+        {
+            if (config.Name != target && !string.IsNullOrEmpty(target)) continue;
+            var password = Configuration.Core.Modules.Security.DecryptSecret($"dockube_ssh_{config.Name}");
+            _client = new SshClient(config.Host, config.Port, config.UserName, password);
+            _client.Connect();
+            Writer.WriteSuccessLine($"Connected to {config.Host}:{config.Port} as {config.UserName}.");
+            var command = _client.RunCommand("sudo poweroff");
+            Writer.WriteLine($"{config.Host} sudo poweroff...");
+            Thread.Sleep(3000);
+            Console.WriteLine(command.Result);
+        }
+        return Ok("Shutdown command sent to all SSH servers.");
+    }
+}
